cfef4d9ad2379b1dbb2f83f901b2864b7b2d95a1
Michelle Tilley <binarymuse@github.com>
2016-10-05 11:58:46 -0700

    Fix async version

--- binding.cc
+++ binding.cc
@@ -10,19 +10,20 @@ using namespace node;
 using Nan::New;
 using Nan::ThrowTypeError;
 
-struct RenderWork {
+class RenderWork {
+public:
+  RenderWork(Nan::Utf8String* markdown, Nan::Callback* callback);
   uv_work_t request;
-  Nan::Utf8String markdown;
+  Nan::Utf8String* markdown;
+  Nan::Callback callback;
   char* result;
-  Persistent<Function> callback;
 };
 
-// struct render_baton {
-//   uv_work_t req;
-//   const char* markdown;
-//   char* result;
-//   Persistent<Function> callback;
-// };
+RenderWork::RenderWork(Nan::Utf8String* markdown, Nan::Callback* callback) {
+  Local<Function> cb = callback->GetFunction();
+  this->markdown = markdown;
+  this->callback.Reset(cb);
+};
 
 void render_sync(const Nan::FunctionCallbackInfo<Value>& args) {
   if (args.Length() < 1) {
@@ -40,39 +41,53 @@ void render_sync(const Nan::FunctionCallbackInfo<Value>& args) {
   args.GetReturnValue().Set(New<String>(result).ToLocalChecked());
 }
 
-// void do_render(uv_work_t* req) {
-//   render_baton* baton = static_cast<render_baton*>(req->data);
-//   const char* markdown = baton->markdown;
-//   char* result = cmark_markdown_to_html(markdown, strlen(markdown), 0);
-//   baton->result = result;
-// }
-//
-// void after_render(uv_work_t *req, int status) {
-//   Isolate* isolate = Isolate::GetCurrent();
-//   HandleScope handleScope(isolate);
-//   const render_baton* baton = (render_baton*) req->data;
-//   const char* result = baton->result;
-//   Local<Value> argv[] = { String::NewFromUtf8(isolate, result) };
-//   Local<Function> cb = Local<Function>::New(isolate, baton->callback);
-//   cb->Call(isolate->GetCurrentContext()->Global(), 1, argv);
-//   baton->callback.Reset(isolate, cb);
-// }
-//
-// void render(const FunctionCallbackInfo<Value>& args) {
-//   if (!check_args(args, 2)) return;
-//   const char* markdown = to_c_string(args[0]);
-//   Isolate* isolate = args.GetIsolate();
-//
-//   render_baton* baton = new render_baton();
-//   baton->req.data = baton;
-//   baton->markdown = markdown;
-//   baton->callback.Reset(isolate, args[1]);
-//   uv_queue_work(uv_default_loop(), &baton->req, do_render, after_render);
-// }
+void do_render(uv_work_t* request) {
+  RenderWork* work = static_cast<RenderWork*>(request->data);
+  Nan::Utf8String* markdown = work->markdown;
+  char* result = cmark_markdown_to_html(**markdown, markdown->length(), 0);
+  work->result = result;
+}
+
+void after_render(uv_work_t* request, int status) {
+  Nan::HandleScope scope;
+  RenderWork* work = static_cast<RenderWork*>(request->data);
+  char* result = work->result;
+  Local<Value> argv[] = { New<String>(result).ToLocalChecked() };
+  work->callback.Call(1, argv);
+}
+
+void render_async(const Nan::FunctionCallbackInfo<Value>& args) {
+  if (args.Length() < 1) {
+    ThrowTypeError("Missing argument 'markdown' at position 0");
+    return;
+  }
+
+  if (!args[0]->IsString()) {
+    ThrowTypeError("Expected argument 'markdown' at position 0 to be a string");
+    return;
+  }
+
+  if (args.Length() < 2) {
+    ThrowTypeError("Missing argument 'callback' at position 1");
+    return;
+  }
+
+  if(!args[1]->IsFunction()) {
+    ThrowTypeError("Expected argument 'callback' at position 1 to be a function");
+    return;
+  }
+
+  Nan::Utf8String* markdown = new Nan::Utf8String(args[0]);
+  Nan::Callback callback(args[1].As<Function>());
+  RenderWork* work = new RenderWork(markdown, &callback);
+  work->request.data = work;
+
+  uv_queue_work(uv_default_loop(), &work->request, do_render, after_render);
+}
 
 void init(Local<Object> exports) {
   exports->Set(New("renderSync").ToLocalChecked(), New<FunctionTemplate>(render_sync)->GetFunction());
-  // NODE_SET_METHOD(exports, "render", render);
+  exports->Set(New("render").ToLocalChecked(), New<FunctionTemplate>(render_async)->GetFunction());
 }
 
 NODE_MODULE(binding, init)

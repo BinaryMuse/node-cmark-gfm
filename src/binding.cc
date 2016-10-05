e85d7d4f8d6be115ff7934c878d18cbe015e5a68
Michelle Tilley <binarymuse@github.com>
2016-10-05 14:58:19 -0700

    Add cmark version to exports

--- src/binding.cc
+++ src/binding.cc
@@ -3,18 +3,20 @@
 #include <uv.h>
 #include <nan.h>
 
+#include "cmark.h"
 #include "markdown.h"
 #include "sync.h"
 #include "async.h"
 
 using Nan::New;
-using v8::FunctionTemplate;
+using v8::Function;
 using v8::Local;
 using v8::Object;
 
 void init(Local<Object> exports) {
-  exports->Set(New("renderHtmlSync").ToLocalChecked(), New<FunctionTemplate>(render_html_sync)->GetFunction());
-  exports->Set(New("renderHtml").ToLocalChecked(), New<FunctionTemplate>(render_html_async)->GetFunction());
+  exports->Set(New("renderHtmlSync").ToLocalChecked(), New<Function>(render_html_sync));
+  exports->Set(New("renderHtml").ToLocalChecked(), New<Function>(render_html_async));
+  exports->Set(New("cmark_version").ToLocalChecked(), New(cmark_version_string()).ToLocalChecked());
 }
 
 NODE_MODULE(binding, init)

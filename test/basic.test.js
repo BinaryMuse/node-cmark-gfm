d194dca4d99cbaca9bfd9098d14dfecf0618a105
Michelle Tilley <binarymuse@github.com>
2016-10-11 17:13:43 -0700

    Add basic tests

---
+++ test/basic.test.js
@@ -0,0 +1,21 @@
+const cmark = require('../')
+
+const assert = require('chai').assert
+
+describe('#renderHtmlSync', () => {
+  it('renders HTML synchronously', () => {
+    assert.htmlEqual(cmark.renderHtmlSync('# Hi'), '<h1>Hi</h1>')
+  })
+})
+
+describe('#renderHtml', () => {
+  it('renders HTML asynchronously', (done) => {
+    let ticked = false
+    cmark.renderHtml('# Hi', (html) => {
+    //   assert.htmlEqual(html, '<h1>Hi</h1>')
+    //   assert.ok(ticked)
+      done()
+    })
+    ticked = true
+  })
+})

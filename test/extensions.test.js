aa44ca9564a68dec9a86bf332a029873f1d1cd64
Michelle Tilley <binarymuse@github.com>
2016-10-12 20:47:01 -0700

    Add tests for remaining extensions

--- test/extensions.test.js
+++ test/extensions.test.js
@@ -30,4 +30,48 @@ describe('extensions', () => {
       assert.htmlEqual(rendered, html)
     })
   })
+
+  describe('strikethrough', () => {
+    it('enables strikethrough', () => {
+      const markdown = dedent`
+      It's all about ~CoffeeScript~ ES2016
+      `.trim()
+
+      const html = `
+      <p>It's all about <del>CoffeeScript</del> ES2016</p>
+      `
+      const rendered = cmark.renderHtmlSync(markdown, {extensions: ['strikethrough']})
+      assert.htmlEqual(rendered, html)
+    })
+  })
+
+  describe('whitelist', () => {
+    it('only allows certain HTML tags to be rendered as raw HTML', () => {
+      const markdown = dedent`
+      <div>What a weird <xmp> tag</div>
+      `.trim()
+
+      const html = `
+      <div>What a weird &lt;xmp> tag</div>
+      `
+
+      const rendered = cmark.renderHtmlSync(markdown, {extensions: ['whitelist']})
+      assert.htmlEqual(rendered, html)
+    })
+  })
+
+  describe('autolink', () => {
+    it('turns URLs into links', () => {
+      const markdown = dedent`
+      Visit us at https://github.com!
+      `.trim()
+
+      const html = `
+      <p>Visit us at <a href="https://github.com">https://github.com</a>!</p>
+      `
+
+      const rendered = cmark.renderHtmlSync(markdown, {extensions: ['autolink']})
+      assert.htmlEqual(rendered, html)
+    })
+  })
 })

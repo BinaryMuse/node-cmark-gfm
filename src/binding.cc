a7daea491deeec840f770e964a9724c3f65b979d
Michelle Tilley <michelle@michelletilley.net>
2019-02-04 14:57:41 -0800

    Replace async renderer with streaming renderer

--- src/binding.cc
+++ src/binding.cc
@@ -3,14 +3,14 @@
 #include "cmark-gfm.h"
 #include "markdown.h"
 #include "sync.h"
-#include "async.h"
+#include "streaming_parser.h"
 
 Napi::Object Init(Napi::Env env, Napi::Object exports) {
   node_cmark_init();
 
   exports.Set("renderHtmlSync", Napi::Function::New(env, render_html_sync));
-  exports.Set("renderHtml", Napi::Function::New(env, render_html_async));
   exports.Set("cmark_version", Napi::String::New(env, cmark_version_string()));
+  StreamingParser::Init(env, exports);
 
   return exports;
 }

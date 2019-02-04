b55cedc449fcc445b91fbc22e29c255e2b7cde7e
Michelle Tilley <michelle@michelletilley.net>
2019-02-04 12:02:32 -0800

    Simplify addon binding

--- src/binding.cc
+++ src/binding.cc
@@ -8,9 +8,9 @@
 Napi::Object Init(Napi::Env env, Napi::Object exports) {
   node_cmark_init();
 
-  exports.Set(Napi::String::New(env, "renderHtmlSync"), Napi::Function::New(env, render_html_sync));
-  exports.Set(Napi::String::New(env, "renderHtml"), Napi::Function::New(env, render_html_async));
-  exports.Set(Napi::String::New(env, "cmark_version"), Napi::String::New(env, cmark_version_string()));
+  exports.Set("renderHtmlSync", Napi::Function::New(env, render_html_sync));
+  exports.Set("renderHtml", Napi::Function::New(env, render_html_async));
+  exports.Set("cmark_version", Napi::String::New(env, cmark_version_string()));
 
   return exports;
 }

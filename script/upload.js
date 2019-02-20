ee7e187f4ca5e6afefd5a1f69afac9f73db33509
HashimotoYT <hashimoto.stream@gmail.com>
2019-02-20 13:08:51 +0200

    Add prebuild outputs

---
+++ script/upload.js
@@ -0,0 +1,12 @@
+const path = require('path')
+const { spawnSync } = require('child_process')
+
+spawnSync(
+  path.join(
+    __dirname,
+    '../node_modules/.bin/prebuild' +
+      (process.platform === 'win32' ? '.cmd' : '')
+  ),
+  ['--upload-all', process.env.GITHUB_AUTH_TOKEN],
+  { stdio: 'inherit' }
+)
